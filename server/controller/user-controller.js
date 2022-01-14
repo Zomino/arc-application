3f279c07b19c3afd8129d72c078e8aa7796ee7e9
Angus Edwards <angus.s.edwards@gmail.com>
2022-01-14 17:56:36 +0000

    bill page rendering

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -6,8 +6,15 @@ const postUsers = async (req, res) => {
   console.log(req.body)
   try {
     const user = req.body;
-    const savedUser = await userModel.create(user);
-    res.status(200).json(savedUser)
+    const clash = await userModel.findOne({email:user.email})
+    if (clash) {
+      console.log('user already exists');
+      res.status(409).send({ error: '409', message: 'User already exists' })
+    }
+    else {
+      const savedUser = await userModel.create(user);
+      res.status(200).json(savedUser)
+    }
   }
   catch (error) {
     console.log(error);
@@ -16,5 +23,14 @@ const postUsers = async (req, res) => {
 }
 
 
+const getUsers = async (req, res) => {
+  const identifier = req.query.identifier;
+  console.log(identifier)
+  const user = await userModel.findOne({email:identifier})
+  console.log(user)
+  res.send(user)
+}
 
-module.exports = { postUsers };
+
+
+module.exports = { postUsers, getUsers };
--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const userController = require('./controller/user-controller');
 
 
 router.post('/', userController.postUsers)
+router.get('/user', userController.getUsers)
 
 
 module.exports = router;
