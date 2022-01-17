84b5f19c17c27fa6bced462452309c996c7740ed
Angus Edwards <angus.s.edwards@gmail.com>
2022-01-17 12:20:40 +0000

    events pulling through successfully

--- server/controller/event-controller.js
+++ server/controller/event-controller.js
@@ -19,5 +19,39 @@ const postEvents = async (req, res) => {
 
 
 
+const getEventsList = async (req, res) => {
+  console.log(req.body.user)
+  try {
+    const user = req.body.user;
+    const eventList = await eventModel.find({});
+    const userEvents = eventList.filter(event => (event.arcs[user]))
+    res.status(200).json(userEvents)
+  }
 
-module.exports = { postEvents }
+  catch (error) {
+    console.log(error);
+    res.sendStatus(500)
+}
+}
+
+
+
+const getEvent = async (req, res) => {
+
+  try {
+    const id = req.body.event
+    const currentEvent = await eventModel.find({_id: id});
+    console.log(currentEvent)
+    res.status(200).json(currentEvent)
+  }
+
+  catch (error) {
+    console.log(error);
+    res.sendStatus(500)
+  }
+}
+
+
+
+
+module.exports = { postEvents, getEventsList, getEvent}
--- server/router.js
+++ server/router.js
@@ -7,6 +7,8 @@ router.post('/usercreate', userController.postUsers)
 router.post('/user', userController.getUser)
 router.get('/userlist', userController.getUsers)
 router.post('/eventcreate', eventController.postEvents)
+router.post('/eventsList', eventController.getEventsList)
+router.post('/currentEvent', eventController.getEvent)
 
 
 
