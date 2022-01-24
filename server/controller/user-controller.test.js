d772c6b1186f8be980ca462ec89d47b69e5c413a
Alex Bolton <boltonalex89@gmail.com>
2022-01-24 15:06:08 +0000

    Refactored and fixed async code on user-controller tests

--- server/controller/user-controller.test.js
+++ server/controller/user-controller.test.js
@@ -18,76 +18,69 @@ describe('User Model tests', () => {
   beforeAll(async () => {
     const url = `mongodb://127.0.0.1/${dbName}`;
     await mongoose.connect(url, { useNewUrlParser: true });
-    await User.deleteMany();
   });
 
-  afterEach(async () => {
-    await User.deleteMany();
+  afterAll(async () => {
+    await mongoose.disconnect();
   });
 
-  it('POST /usercreate', async () => {
-    // Create and post our new user
-    const userDetails = {
-      firstName: 'Tex',
-      lastName: 'Howdy',
-      email: 'tex.howdy@examplia.com',
-      password: 'password',
-    };
-    await request.post('/userCreate').send(userDetails);
+  describe('User creation endpoints', () => {
+    beforeAll(async () => {
+      await User.deleteMany();
+    });
 
-    // Find in test DB and compare
-    const user = await User.findOne({ firstName: 'Tex' });
-    expect(user).toMatchObject(userDetails);
-  });
+    it('POST /usercreate', async () => {
+      // Create and post our new user
+      const userDetails = {
+        firstName: 'Tex',
+        lastName: 'Howdy',
+        email: 'tex.howdy@examplia.com',
+        password: 'password',
+      };
+      await request.post('/userCreate').send(userDetails);
 
-  it('POST /user', async () => {
-    // Check we don't have the user already
-    const nullUser = await User.findOne({ firstName: 'Tex' });
-    expect(nullUser).toBeNull();
+      // Find in test DB and compare
+      const user = await User.findOne({ firstName: 'Tex' });
+      expect(user).toMatchObject(userDetails);
+    });
+  })
 
-    // Create our user in test DB
-    const userDetails = {
-      firstName: 'Tex',
-      lastName: 'Howdy',
-      email: 'tex.howdy@examplia.com',
-      password: 'password',
-    };
-    await User.create(userDetails);
-
-    // Request for user and compare
-    const user = await request.post('/user').send({email: userDetails.email});
-    expect(user.body).toMatchObject(userDetails);
-
-  });
-
-  it('GET /userlist', async () => {
-    // Check we don't have the user already
-    const nullUser = await User.findOne({ firstName: 'Tex' });
-    expect(nullUser).toBeNull();
-
-    // Create our users in test DB
+  describe('User retrieval endpoints', () => {
     const userOneDetails = {
       firstName: 'Tex',
       lastName: 'Howdy',
       email: 'tex.howdy@examplia.com',
       password: 'password1',
     };
-    await User.create(userOneDetails);
     const userTwoDetails = {
       firstName: 'Tasha',
       lastName: 'Bonjour',
       email: 'tash.bonjour@examplia.com',
       password: 'password2',
     };
-    await User.create(userTwoDetails);
 
-    // Request for user and compare
-    const users = await request.get('/userlist');
+    beforeAll(async () => {
+      await User.deleteMany();
+      await User.create(userOneDetails);
+      await User.create(userTwoDetails);
+    });
 
-    // Check users match
-    expect(users.body[0]).toMatchObject(userOneDetails);
-    expect(users.body[1]).toMatchObject(userTwoDetails);
-  });
+    it('POST /user', async () => {
+      // Request for user and compare
+      const user = await request.post('/user').send({ email: userOneDetails.email });
+      expect(user.body).toMatchObject(userOneDetails);
+    });
+
+    it('GET /userlist', async () => {
+      // Request for user and compare
+      const users = await request.get('/userlist');
+
+      // Check users match
+      expect(users.body[0]).toMatchObject(userOneDetails);
+      expect(users.body[1]).toMatchObject(userTwoDetails);
+    });
+
+  })
 });
 
 export default {};
