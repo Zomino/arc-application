5e9e8c00da0c2a3718c4c6461c90915f7173f820
Zou Minowa <zouminowa@gmail.com>
2022-01-24 09:50:39 +0000

    remove unnecessary ref to db in server index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import router from './router';
-import db from './db';
 
 const app = express();
 const port = 3000;
@@ -11,6 +10,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(port, () => {
-  db;
   console.log(`Server listening at http://localhost:${port}`);
 });
