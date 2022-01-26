13b5d76005854cd1630c48c8ed66e7f7be5cc218
Zou Minowa <zouminowa@gmail.com>
2022-01-26 10:34:32 +0000

    refactor(client): refactor App.tsx

--- client/App.tsx
+++ client/App.tsx
@@ -1,18 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MyTabs from './Tab';
 import Entry from './components/EntryComponent';
 
-function App() {
+export default function App() {
   const [user, setUser] = useState('');
 
   return (
     <NavigationContainer>
-      {user ?
-        <MyTabs user={user} setUser={setUser} /> :
-        <Entry setUser={setUser} />}
+      {user
+        ? <MyTabs user={user} setUser={setUser} />
+        : <Entry setUser={setUser} />}
     </NavigationContainer>
   );
 }
-
-export default App;
